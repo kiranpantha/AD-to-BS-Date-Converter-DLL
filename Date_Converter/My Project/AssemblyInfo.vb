7ca5faeb31d3bcaaa0a6792b82001d731b47a392
Kiran Pantha <info@kiranpantha.com.np>
2021-05-19 01:17:19 +0545

    Github Migrate from Computer

---
+++ Date_Converter/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DateConverter")> 
+<Assembly: AssemblyDescription("Class of date Converter")> 
+<Assembly: AssemblyCompany("kiransoft Nepal")> 
+<Assembly: AssemblyProduct("ADTOBS")> 
+<Assembly: AssemblyCopyright("Copyright © kiransoft Nepal 2013")> 
+<Assembly: AssemblyTrademark("Kiransoft Nepal  | www.kiranpantha.com.np |")> 
+
+<Assembly: ComVisible(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("78ed606b-d135-47e4-b8a5-a20e845b85f9")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
